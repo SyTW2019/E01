3f6710b9bbfc6d72eb0eb4795d72ec4bdcbd4b3d
Daikikun <kaerit@hotmail.com>
2019-11-15 21:13:29 +0000

    borrado el home y retocado detalles que faltaban

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -11,7 +11,6 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
@@ -30,7 +29,6 @@ import { AuthGuard } from './guards/auth.guard';
 
 // Array for the routing
 const appRoutes: Routes = [
-  { path: 'home', component: HomeComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent},
   { path: '', component: LoginComponent },
@@ -47,7 +45,6 @@ const appRoutes: Routes = [
     NavbarComponent,
     LoginComponent,
     RegisterComponent,
-    HomeComponent,
     DashboardComponent,
     ProfileComponent,
     FooterComponent,
