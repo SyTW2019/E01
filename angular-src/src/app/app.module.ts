c2968849ee9fb2002fa6353870645117d36911d7
Borja García Glez <alu0100897562@ull.edu.es>
2019-11-16 19:08:44 +0000

    Añadida estructura de testing Cucumber

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -24,6 +24,10 @@ import { FlashMessagesModule } from 'angular2-flash-messages';
 import { JwtModule } from '@auth0/angular-jwt';
 import { AuthGuard } from './guards/auth.guard';
 import { FooterComponent } from './footer/footer.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { EffectsModule } from '@ngrx/effects';
+import { AppEffects } from './app.effects';
 
 // Array for the routing
 const appRoutes: Routes = [
@@ -64,7 +68,15 @@ const appRoutes: Routes = [
           return localStorage.getItem('id_token');
         }
       }
-    })
+    }),
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true
+      }
+    }),
+    EffectsModule.forRoot([AppEffects])
   ],
   providers: [
     ValidateService,
